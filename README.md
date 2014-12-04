f89b67047d2a42304fc9f3d7cc9559157f36c21c
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-04 12:18:23 -0500

    TFront-end package and task running stuff. Got Gulp in and running well. Renamed a lot of files for better organization. Installed Bower packages.

--- README.md
+++ README.md
@@ -3,6 +3,29 @@ MEAN_stack_starter
 
 A ready-to-go initialized set of MEAN + SASS + Bower + Gulp + CSS Normalize.
 
+<h2>File Structure</h2>
+
+I've decided to go with a modularized, by-the-function style of file organization. Looks something like this:
+
+public/
+-- shared/                  // acts as reusable components or partials of our site
+-- components/              // each component is treated as a mini Angular app
+---- nerds/
+------ views/
+-------- nerds.index.html
+-------- nerds.show.html
+------ nerds.ctrl.js
+------ nerds.module.js
+------ nerds.service.js
+-- assets/
+---- img/                   // Images and icons for your app
+---- css/                   // All styles and style related files (SCSS or LESS files)
+---- js/                    // JavaScript files written for your app that are not for angular
+---- libs/                  // Third-party libraries such as jQuery, Moment, Underscore, etc.
+-- app.js
+-- app.routes.js
+-- index.html
+
 
 <h2>In order to use me, update:</h2>
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,58 @@
-var gulp        = require('gulp'),     
-    sass        = require('gulp-ruby-sass') ,
-    autoprefix  = require('gulp-autoprefixer') ,
-    notify      = require("gulp-notify") ,
-    bower       = require('gulp-bower');
+var gulp          = require('gulp'),
+    sass          = require('gulp-ruby-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    minifycss     = require('gulp-minify-css'),
+    jshint        = require('gulp-jshint'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    rename        = require('gulp-rename'),
+    concat        = require('gulp-concat'),
+    notify        = require('gulp-notify'),
+    cache         = require('gulp-cache'),
+    livereload    = require('gulp-livereload'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    del           = require('del');
 
-var config = {
-  sassPath: './resources/sass',
-  bowerDir: './public/libs'
-}
+// var config = {
+//   sassPath: './resources/sass',
+//   bowerDir: './public/libs'
+// }
 
 gulp.task('default', function() {
-  // place code for your default task here
+    gulp.start('styles', 'scripts');
 });
+
+gulp.task('clean', function(cb) {
+    // del(['public/assets/css/', 'public/assets/js', 'public/assets/img'], cb)
+});
+
+gulp.task('styles', function() {
+  return gulp.src('public/assets/css/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(sourcemaps.write("./"))    
+    
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    // Throw our production version
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('public/assets/css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('public/assets/css/'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+
+gulp.task('scripts', function() {
+  return gulp.src('public/assets/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// gulp.task('watch', function() {
+//   gulp.watch('')
+// });
