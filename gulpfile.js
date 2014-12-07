36ec641b9b9abb78cbaf826711237053c0c72a28
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-07 09:35:22 -0500

    Lots of gulpfile work.

--- gulpfile.js
+++ gulpfile.js
@@ -10,43 +10,39 @@ var gulp          = require('gulp'),
     notify        = require('gulp-notify'),
     cache         = require('gulp-cache'),
     livereload    = require('gulp-livereload'),
-    sourcemaps    = require('gulp-sourcemaps'),
-    del           = require('del');
+    sourcemaps    = require('gulp-sourcemaps');
 
-// var config = {
-//   sassPath: './resources/sass',
-//   bowerDir: './public/libs'
-// }
+function errorLog (error) {
+  console.error.bind(error);
+  this.emit('end');
+}
 
 gulp.task('default', function() {
     gulp.start('styles', 'scripts', 'watch');
 });
 
-gulp.task('clean', function(cb) {
-    // del(['public/assets/css/', 'public/assets/js', 'public/assets/img'], cb)
-});
-
 gulp.task('styles', function() {
   return gulp.src('public/**/*.scss')
-    .pipe(sourcemaps.init())
-      .pipe(sass())
+    .pipe(sass({sourcemap: true}))
+    .on('error', errorLog)
+    .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-      .pipe(minifycss())
       .pipe(concat('style.min.css'))
-    .pipe(sourcemaps.write('./'))    
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('public/assets/css/'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 
 gulp.task('scripts', function() {
-  return gulp.src(['public/**/*.module.js', 'public/app.routes.js', 'public/app.js', 'public/components/**/*.js'])
+  return gulp.src(['public/**/*.module.js', 'public/app.routes.js', 'public/app.js', 'public/components/**/*.js', 'public/shared/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(sourcemaps.init())
       .pipe(concat('main.min.js'))
       .pipe(uglify())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
+    .on('error', errorLog)
     .pipe(gulp.dest('public/assets/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
